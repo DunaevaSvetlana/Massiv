427d44704ca0cb961e28a12a75dbec7ccf22105b
DunaevaSvetlana <solnste1993@mail.ru>
2020-04-28 22:57:42 +0400

    Initial commit

---
+++ src/main/java/Massiv.java
@@ -0,0 +1,11 @@
+public class Massiv {
+    public static void main(String[] args) {
+        int[] numbers = {11, 22, 33, 44, 55};
+        System.out.println("numbers[0] = " + numbers[0]);
+        System.out.println("numbers[1] = " + numbers[1]);
+        System.out.println("numbers[2] = " + numbers[2]);
+        System.out.println("numbers[3] = " + numbers[3]);
+        System.out.println("numbers[4] = " + numbers[4]);
+    }
+
+}
